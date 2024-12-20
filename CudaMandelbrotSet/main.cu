a2d4b270547b130916d5c5e0255c9550f7af4f0f
Vasiliy <79756790+Lostmindd@users.noreply.github.com>
2024-12-20 22:39:43 +0300

    refactoring

--- CudaMandelbrotSet/main.cu
+++ CudaMandelbrotSet/main.cu
@@ -5,45 +5,22 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
+int screen_width = 1024;
+int screen_height = 640;
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 SDL_Texture* texture = nullptr;
 
-// Инициализирует окно, рендерер и текстуру
-bool initWindow()
+// Initializes the window, renderer and texture
+void initWindow()
 {
-    bool ok = true;
-
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        ok = false;
-    }
-
-    window = SDL_CreateWindow("CudaMandelbrot", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (!window)
-    {
-        ok = false;
-    }
-
+    window = SDL_CreateWindow("CudaMandelbrot", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
-    {
-        ok = false;
-    }
-
-    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
-    if (!texture)
-    {
-        ok = false;
-    }
-
-    return ok;
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, screen_width, screen_height);
 }
 
-// Возвращает цвет в множестве Мандельброта
+// Returns the color in the Mandelbrot set for x and y
 __device__ Uint8 getMandelbrotColor(double x, double y)
 {
     double z1 = 0, z2 = 0;
@@ -60,7 +37,9 @@ __device__ Uint8 getMandelbrotColor(double x, double y)
     return 255;
 }
 
-__global__ void computeMandelbrot(Uint8* __restrict__ colors, double min_x, double min_y, const double x_diff, const double y_diff)
+// Compute the colors of each pixel for the Mandelbrot set on the GPU
+__global__ void computeMandelbrot(Uint8* __restrict__ colors, double min_x, double min_y, 
+    const double x_diff, const double y_diff, const int SCREEN_WIDTH, const int SCREEN_HEIGHT)
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
@@ -73,30 +52,31 @@ __global__ void computeMandelbrot(Uint8* __restrict__ colors, double min_x, doub
     }
 }
 
-// Генерирует множество Мандельброта на экране
+// Renders the Mandelbrot set on the screen
 void renderMandelbrotSet(double min_x, double max_x, double min_y, double max_y)
 {
-    const double x_diff = (max_x - min_x) / SCREEN_WIDTH;
-    const double y_diff = (max_y - min_y) / SCREEN_HEIGHT;
+    const double x_diff = (max_x - min_x) / screen_width;
+    const double y_diff = (max_y - min_y) / screen_height;
 
-    Uint8* device_colors;
-    cudaMalloc((void**)&device_colors, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint8));
+    Uint8* colors_on_gpu;
+    cudaMalloc((void**)&colors_on_gpu, screen_width * screen_height * sizeof(Uint8));
 
-    dim3 blockSize(16, 16);
-    dim3 gridSize((SCREEN_WIDTH + 15) / 16, (SCREEN_HEIGHT + 15) / 16);
-    computeMandelbrot <<<gridSize, blockSize >>> (device_colors, min_x, min_y, x_diff, y_diff);
+    dim3 blockSize(32, 32);
+    dim3 gridSize((screen_width + 31) / 32, (screen_height + 31) / 32);
+    computeMandelbrot <<<gridSize, blockSize>>> (colors_on_gpu, min_x, min_y, x_diff, y_diff, screen_width, screen_height);
+
+    Uint8* colors_on_cpu = new Uint8[screen_width * screen_height];
+    //copies to cpu only when all blocks complete their work
+    cudaMemcpy(colors_on_cpu, colors_on_gpu, screen_width * screen_height * sizeof(Uint8), cudaMemcpyDeviceToHost);
 
     Uint32* pixels = nullptr;
-    int pitch;
+    int pitch; //length of one texture line in bytes
     SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch);
 
-    Uint8* host_colors = new Uint8[SCREEN_WIDTH * SCREEN_HEIGHT];
-    cudaMemcpy(host_colors, device_colors, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint8), cudaMemcpyDeviceToHost);
-
     #pragma omp parallel for colapse(2)
-    for (int i = 0; i < SCREEN_HEIGHT; i++) {
-        for (int j = 0; j < SCREEN_WIDTH; j++) {
-            Uint8 color = host_colors[i * SCREEN_WIDTH + j];
+    for (int i = 0; i < screen_height; i++) {
+        for (int j = 0; j < screen_width; j++) {
+            Uint8 color = colors_on_cpu[i * screen_width + j];
             Uint32 pixelColor = 0xFF000000 | ((color * 5) % 256 << 16) | ((color * 7) % 256 << 8) | ((color * 11) % 256);
             pixels[i * (pitch / 4) + j] = pixelColor;
         }
@@ -106,17 +86,33 @@ void renderMandelbrotSet(double min_x, double max_x, double min_y, double max_y)
     SDL_RenderCopy(renderer, texture, nullptr, nullptr);
     SDL_RenderPresent(renderer);
 
-    cudaFree(device_colors);
-    delete[] host_colors;
+    cudaFree(colors_on_gpu);
+    delete[] colors_on_cpu;
+}
+
+// Ends the program
+void quit()
+{
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char* argv[])
 {
     ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+    if (argc == 3) 
+    {
+        screen_width = atoi(argv[1]);  
+        screen_height = atoi(argv[2]); 
+    }
+
     SDL_Init(SDL_INIT_EVERYTHING);
     initWindow();
 
-    double curent_min_x = -1, curent_max_x = 1, curent_min_y = -1, curent_max_y = 1;
+    double curent_min_x = -2, curent_max_x = 1, curent_min_y = -1.5, curent_max_y = 1.5;
     double curent_move_step = (curent_max_x - curent_min_x) / 10;
 
     renderMandelbrotSet(curent_min_x, curent_max_x, curent_min_y, curent_max_y);
@@ -128,8 +124,9 @@ int main(int argc, char* argv[])
         {
             bool rerender_needed = false;
 
-            if (SDL_MOUSEWHEEL == window_event.type)
+            switch (window_event.type)
             {
+            case SDL_MOUSEWHEEL:
                 if (window_event.wheel.y == -1)
                 {
                     curent_min_x -= curent_move_step;
@@ -145,42 +142,40 @@ int main(int argc, char* argv[])
                     curent_max_y -= curent_move_step;
                 }
                 rerender_needed = true;
-                curent_move_step = (curent_max_x - curent_min_x) / 10;
-            }
+                curent_move_step = (curent_max_x - curent_min_x) / 8;
+                break;
 
-            if (SDL_KEYDOWN == window_event.type)
-            {
-                if (window_event.key.keysym.sym == SDLK_UP) {
+            case SDL_KEYDOWN:
+                switch (window_event.key.keysym.sym)
+                {
+                case SDLK_UP:
                     curent_min_y -= curent_move_step;
                     curent_max_y -= curent_move_step;
-                }
-                else if (window_event.key.keysym.sym == SDLK_DOWN) {
+                    break;
+                case SDLK_DOWN:
                     curent_min_y += curent_move_step;
                     curent_max_y += curent_move_step;
-                }
-                else if (window_event.key.keysym.sym == SDLK_LEFT) {
+                    break;
+                case SDLK_LEFT:
                     curent_min_x -= curent_move_step;
                     curent_max_x -= curent_move_step;
-                }
-                else if (window_event.key.keysym.sym == SDLK_RIGHT) {
+                    break;
+                case SDLK_RIGHT:
                     curent_min_x += curent_move_step;
                     curent_max_x += curent_move_step;
+                    break;
+                case SDLK_ESCAPE:
+                    quit();
                 }
                 rerender_needed = true;
-                curent_move_step = (curent_max_x - curent_min_x) / 10;
-            }
+                curent_move_step = (curent_max_x - curent_min_x) / 8;
+                break;
 
+            case SDL_QUIT:
+                quit();
+            }
             if (rerender_needed)
                 renderMandelbrotSet(curent_min_x, curent_max_x, curent_min_y, curent_max_y);
-
-            if (SDL_QUIT == window_event.type)
-                break;
         }
     }
-
-    SDL_DestroyTexture(texture);
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    SDL_Quit();
-    return EXIT_SUCCESS;
 }
